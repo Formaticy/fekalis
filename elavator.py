c1120a4c22ce12de0e29a908bf17ea22bfe70139
davidosick <a-shavlidze@mail.ru>
2025-02-23 23:35:50 +0300

    merge

--- elavator.py
+++ elavator.py
@@ -2,10 +2,11 @@ class Elevator:
     """
     Класс для расчета подъема груза на этаж
     """
-    def __init__(self, capacity):
-        self.capacity = capacity
     pass
 
+    def __init__(self, capacity):
+        self.capacity = capacity
+        
     def manual_up(price = 300):
         pass
 
